aca0bf583cee84826ab4ea01a45fc47c395c392b
Rasenkai <rasenkai99@gmail.com>
2021-12-14 14:48:31 +0000

    base: IS_DEBUGGABLE -> IS_ENG
    * based on commit 4e750b88ad3ef24e33248de09bbddbfac517423b
    -----
    git grep -l 'Build.IS_DEBUGGABLE' | xargs sed -i 's/Build.IS_DEBUGGABLE/Build.IS_ENG/g'
    
    Change-Id: I16d68f2c5625913d5f55026b9d4f2e9a47accdc3
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -1005,7 +1005,7 @@ public final class BatteryService extends SystemService {
         pw.println("  reset [-f]");
         pw.println("    Unfreeze battery state, returning to current hardware values.");
         pw.println("    -f: force a battery change broadcast be sent, prints new sequence.");
-        if (Build.IS_DEBUGGABLE) {
+        if (Build.IS_ENG) {
             pw.println("  disable_charge");
             pw.println("    Suspend charging even if plugged in. ");
         }
@@ -1164,7 +1164,7 @@ public final class BatteryService extends SystemService {
     }
 
     private void suspendBatteryInput() {
-        if (!Build.IS_DEBUGGABLE) {
+        if (!Build.IS_ENG) {
             throw new SecurityException(
                     "battery suspend_input is only supported on debuggable builds");
         }
